145f708a155dcb95c8f10b74c169f474e35abf52
guochenye <guochenye@bytedance.com>
2022-11-27 02:04:20 +0800

    fix:修改idl

--- user_core.thrift
+++ user_core.thrift
@@ -1,13 +1,19 @@
+include "base.thrift"
+
 namespace go user
 
 struct RegisterRequest {
-    1: required string name;
-    2: required string gender;
-    3: required string phone_num;
+    1: required string Name;
+    2: required string Gender;
+    3: required string PhoneNum;
+
+    255: required base.Base Base;
 }
 
 struct RegisterResponse {
-    1: required string user_id;
+    1: required string UserID;
+
+    255: required base.BaseResp BaseResp;
 }
 
 service User {
